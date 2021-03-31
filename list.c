b0a0dd1597a36d85de181d09d9c14131d247b662
Replit user <replituser@example.com>
2021-03-31 22:47:38 +0000

    new try

--- list.c
+++ list.c
@@ -113,7 +113,7 @@ void * popCurrent(List * list) {
   Node *aux = list->current;
   if(list->current == list->head){
     list->head=list->current->next;
-    list->current = NULL;
+    list->head->prev = NULL;
   }
   else if(list->current == list->tail){
 
