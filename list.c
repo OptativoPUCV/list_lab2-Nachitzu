29e18b22fbebe41693a35c8a726794ab14c1f8d9
Replit user <replituser@example.com>
2021-03-31 19:21:17 +0000

    new try

--- list.c
+++ list.c
@@ -88,7 +88,7 @@ void pushCurrent(List * list, const void * data) {
    list->head->next = newNode;
    newNode->prev = list->head;
    list->tail = newNode;
-   list->tail->next = newNode;
+   list->tail->next = 0;
   }
   else{
     printf(" list->current es NULL");
