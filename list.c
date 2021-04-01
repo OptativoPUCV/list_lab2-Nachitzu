db9dc89293a120a01226ae8450d73db8470239ae
Replit user <replituser@example.com>
2021-04-01 01:32:32 +0000

    new try

--- list.c
+++ list.c
@@ -110,21 +110,19 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-  Node *aux = list->current;
   if(list==NULL) return NULL;
+  Node *aux = list->current;
   if(list->current == list->head){
     list->head=list->current->next;
     list->head->prev = NULL;
   }
   else if(list->current == list->tail){
-    list->tail->prev = list->tail;
+    list->tail = list->tail->prev;
     list->tail->next = NULL;
-    
-    
   }
   else{
     list->current->next->prev = list->current->prev;
-    //list->current->prev->next = list->current->next;
+    list->current->prev->next = list->current->next;
   }
   return(void*) (aux->data);
 
